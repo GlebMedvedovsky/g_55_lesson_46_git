6dfd85b7149335e310c3da5b62d034ded51cb8b7
GlebMedvedovsky <post@medy-trans.de>
2025-01-29 11:26:24 +0100

    Task 2

--- src/main/java/app/task_02/Main.java
+++ src/main/java/app/task_02/Main.java
@@ -2,7 +2,7 @@ package app.task_02;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Task 2");
-    }
+    public static void main(String[] args) {System.out.println("Task 2 update");}
+
+
 }
